c0b5a18c0a93e598622c5d502cd925f88fc4509a
Blaine Buxton <self@blainebuxton.com>
2017-07-16 12:16:01 -0400

    minor additions

--- src/main/java/net/blabux/midigen/midi/PitchBend.java
+++ src/main/java/net/blabux/midigen/midi/PitchBend.java
@@ -1,4 +1,11 @@
 package net.blabux.midigen.midi;
+
+import java.util.function.Consumer;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
 /**
  * http://www.midikits.net/midi_analyser/pitch_bend.htm
  * https://music.stackexchange.com/questions/47995/midi-pitch-bend-vs-midi-tuning-standard-microtones
@@ -33,5 +40,17 @@ public class PitchBend {
 		result[0] = (byte)(amount & 0x7F);
 		result[1] = (byte)((amount >> 7) & 0x7F);
 		return result;
+	}
+	
+	public void setToWholeToneBend(Consumer<MidiEvent> consumer, int channel, long tick) throws InvalidMidiDataException {
+		//Bn 65 00 ; 101  00 MSB
+		//Bn 64 00 ; 100 00 LSB
+		//Bn 06 18 ; 06 24 MSB
+		//Bn 26 00 ; 38 00 LSB
+		consumer.accept(new MidiEvent(new ShortMessage(ShortMessage.CONTROL_CHANGE, channel, 0x65, 0x00), tick++));
+		consumer.accept(new MidiEvent(new ShortMessage(ShortMessage.CONTROL_CHANGE, channel, 0x64, 0x00), tick++));
+		consumer.accept(new MidiEvent(new ShortMessage(ShortMessage.CONTROL_CHANGE, channel, 0x06, 0x04), tick++));
+		consumer.accept(new MidiEvent(new ShortMessage(ShortMessage.CONTROL_CHANGE, channel, 0x26, 0x00), tick++));
+		
 	}
 }
--- src/test/java/net/blabux/midigen/midi/PitchBendTest.java
+++ src/test/java/net/blabux/midigen/midi/PitchBendTest.java
@@ -1,6 +1,13 @@
 package net.blabux.midigen.midi;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.Track;
 
 import org.junit.Test;
 
@@ -9,35 +16,35 @@ public class PitchBendTest {
 	@Test
 	public void testA4Midi() {
 		double midi = PitchBend.freqToMidi(440);
-		assertEquals((int)6900, (int)(midi * 100));
+		assertEquals((int) 6900, (int) (midi * 100));
 	}
-	
+
 	@Test
 	public void testA4Freq() {
 		double freq = PitchBend.midiToFreq(69);
-		assertEquals((int)44000, (int)(freq * 100));
+		assertEquals((int) 44000, (int) (freq * 100));
 	}
-	
+
 	@Test
 	public void testMidi() {
 		double midi = PitchBend.freqToMidi(8372.0180896192);
-		assertEquals((int)12000, (int)(midi * 100));
+		assertEquals((int) 12000, (int) (midi * 100));
 	}
-	
+
 	@Test
 	public void testFreq() {
 		double freq = PitchBend.midiToFreq(120);
-		assertEquals((int)8372018, (int)(freq * 1000));
+		assertEquals((int) 8372018, (int) (freq * 1000));
 	}
 
 	@Test
 	public void pitchBend() {
-		double freq = 440 * 3 / 2; //perfect fifth
+		double freq = 440 * 3 / 2; // perfect fifth
 		PitchBend pb = new PitchBend(freq);
 		assertEquals(76, pb.midiNote);
 		assertEquals(8352, pb.amount);
 	}
-	
+
 	@Test
 	public void pitchBendMidi() {
 		double freq = PitchBend.midiToFreq(78);
@@ -45,21 +52,29 @@ public class PitchBendTest {
 		assertEquals(78, pb.midiNote);
 		assertEquals(8192, pb.amount);
 	}
-	
+
 	@Test
 	public void getAmountForMidiPB() {
-		double freq = 440 * 3 / 2; //perfect fifth
+		double freq = 440 * 3 / 2; // perfect fifth
 		PitchBend pb = new PitchBend(freq);
 		byte[] amount = pb.getAmountForMidiPB();
 		assertEquals(0x20, amount[0]);
 		assertEquals(0x41, amount[1]);
 	}
-	
+
 	@Test
 	public void getAmountForMidiPBCenter() {
 		PitchBend pb = new PitchBend(440);
 		byte[] amount = pb.getAmountForMidiPB();
 		assertEquals(0x00, amount[0]);
 		assertEquals(0x40, amount[1]);
+	}
+
+	@Test
+	public void pitchBendRange() throws InvalidMidiDataException {
+		PitchBend pb = new PitchBend(440);
+		List<MidiEvent> events = new ArrayList<>();
+		pb.setToWholeToneBend(events::add, 0, 0);
+		assertEquals(4, events.size());
 	}
 }
